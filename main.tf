29f78e04be7506fc9029ec832eb6f9bf00ef07b5
Gary-Dutton <training@a4artificial-i4intelligence.com>
2023-05-16 13:23:08 +0000

    New commit

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.34.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+resource "azurerm_resource_group" "contoso_rg" {
+  name     = "contoso_rg"
+  location = "UK South"
+}
